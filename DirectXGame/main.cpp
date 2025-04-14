572c14cb8e406e1ee3bee58b5431167cf4d369bb
sato-yui-13 <k024g0002@g.neec.ac.jp>
2025-04-14 15:59:37 +0900

    aho

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,54 @@
+#include "KamataEngine.h"
+#include <Windows.h>
+#include"GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"LE2D_13_サトウ_ユイ_AL3");
+
+
+		DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+		//ゲームシーンのいインスタンス生成
+	    GameScene* gameScene = new GameScene();
+		//ゲームシーンの初期化
+	    gameScene->Initialize();
+
+
+	// mainループ
+	while (true) {
+
+	
+		//エンジンの更新
+		if (KamataEngine::Update()) {
+
+			break;
+		}
+
+		//ゲームシーンの更新
+		gameScene->Update();
+	
+		//描画開始
+		dxCommon->PreDraw();
+
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		//描画終了
+		dxCommon->PostDraw();
+	}
+
+	//ゲームシーンの解散
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+	
+	return 0;
+
+}
